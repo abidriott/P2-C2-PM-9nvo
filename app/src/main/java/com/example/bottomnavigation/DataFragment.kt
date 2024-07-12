ef0d607ebdbf53c6c4af332dac9a04c9332fda60
abidriott <abigailgutierrezsly@gmail.com>
2024-07-12 13:03:07 -0700

    a lot configurations

--- app/src/main/java/com/example/bottomnavigation/DataFragment.kt
+++ app/src/main/java/com/example/bottomnavigation/DataFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.bottomnavigation.DataBase.Alumno
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+private lateinit var btnGuardar: Button
+
 
 /**
  * A simple [Fragment] subclass.
@@ -17,7 +22,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class DataFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -35,7 +39,14 @@ class DataFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_data, container, false)
+    btnBuscar.setOnClickLIstener{
+        if(txtMatricula)
     }
+    }
+
+    var alumnos:Alumno
+    alumnos=Alumno();
+    alumnos.nombre=txtNombre.text
 
     companion object {
         /**
